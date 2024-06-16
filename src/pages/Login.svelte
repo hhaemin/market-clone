e7de85bdc7b0b134661f698d45576d06a8e8949b
hhaemin <ahrgoals@naver.com>
2024-06-16 22:55:24 +0900

    유저인증까지 파이어베이스로 구현

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,15 +1,50 @@
-<form id="login-form" action="/signup" method="POST">
-  <div>로그인하기</div>
-  <div>
-    <label for="id">아이디</label>
-    <input type="text" id="id" name="id" required />
-  </div>
-  <div>
-    <label for="password">패스워드</label>
-    <input type="password" id="password" name="password" required />
-  </div>
-  <div>
-    <button type="submit">로그인</button>
-  </div>
-  <div id="info"></div>
-</form>
+<script>
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { user$ } from "../store";
+
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      user$.set(user);
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+</script>
+
+<div>
+  <div>로그인 하기</div>
+  <button class="login-btn" on:click={loginWithGoogle}>
+    <img
+      class="google-img"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/160px-Google_%22G%22_logo.svg.png"
+      alt=""
+    />
+    <div>Google로 시작하기</div>
+    <div />
+  </button>
+</div>
+
+<style>
+  .login-btn {
+    width: 200px;
+    height: 50px;
+    border: 1px solid grey;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    border-radius: 3px;
+  }
+
+  .google-img {
+    width: 20px;
+  }
+</style>
